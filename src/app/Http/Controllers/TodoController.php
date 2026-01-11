ac30eeb9d0b0fa66210843b134d22dcbec37fb61
yoko_sakamaki <yoko_sakamaki@fandmnet.com>
2026-01-11 15:37:05 +0900

    Viewとカテゴリの構築

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,20 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        $todos = Todo::all();
-        return view('index', compact('todos'));
+        $todos = Todo:all();
+          return view('index', compact('todos'));
     }
 
     public function store(TodoRequest $request)
     {
         $todo = $request->only(['content']);
         Todo::create($todo);
+
         return redirect('/')->with('message', 'Todoを作成しました');
     }
 
@@ -32,6 +34,7 @@ class TodoController extends Controller
     public function destroy(Request $request)
     {
         Todo::find($request->id)->delete();
+
         return redirect('/')->with('message', 'Todoを削除しました');
     }
 }
